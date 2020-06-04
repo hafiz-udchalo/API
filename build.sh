df444551e8957f31e167b5b7f6b1011e6c721412
hafiz-udchalo <61221372+hafiz-udchalo@users.noreply.github.com>
2020-06-04 15:57:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,13 +18,13 @@ sed -i=' ' "s/<NODE_ENV>/$1/g" template.yaml
 
 export BUCKET="api-gateway-$1"
 
-aws s3 mb s3://$BUCKET
+#aws s3 mb s3://$BUCKET
 
 # Uploads files to S3 bucket and creates CloudFormation template
-sam package --template-file template.yaml --s3-bucket $BUCKET --output-template-file package.yaml
+#sam package --template-file template.yaml --s3-bucket $BUCKET --output-template-file package.yaml
 
 # Deploys your stack   
-sam deploy --template-file package.yaml --stack-name $STACK --capabilities CAPABILITY_IAM
+#sam deploy --template-file package.yaml --stack-name $STACK --capabilities CAPABILITY_IAM
 
 
 
